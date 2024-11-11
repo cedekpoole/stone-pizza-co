045db8a1ae5ce8bd708dd0c27154ae158bf118a4
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-11 18:40:01 +0000

    create react-router-dom loader and add it to order route

--- src/features/order/Order.jsx
+++ src/features/order/Order.jsx
@@ -1,45 +1,15 @@
+// test id: IIDSAT; CQE92U
+
+import { useLoaderData } from "react-router-dom";
+import { getOrder } from "../../services/apiMenu";
 import {
   calcMinutesLeft,
   formatCurrency,
   formatDate,
 } from "../../utils/helpers";
 
-const order = {
-  id: "ABCDEF",
-  customer: "Jonas",
-  phone: "123456789",
-  address: "Arroios, Lisbon , Portugal",
-  priority: true,
-  estimatedDelivery: "2027-04-25T10:00:00",
-  cart: [
-    {
-      pizzaId: 7,
-      name: "Napoli",
-      quantity: 3,
-      unitPrice: 16,
-      totalPrice: 48,
-    },
-    {
-      pizzaId: 5,
-      name: "Diavola",
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-    {
-      pizzaId: 3,
-      name: "Romana",
-      quantity: 1,
-      unitPrice: 15,
-      totalPrice: 15,
-    },
-  ],
-  position: "-9.000,38.000",
-  orderPrice: 95,
-  priorityPrice: 19,
-};
-
 function Order() {
+  const order = useLoaderData();
   // Everyone can search for all orders, so for privacy reasons we're gonna gonna exclude names or address, these are only for the restaurant staff
   const {
     id,
@@ -79,6 +49,13 @@ function Order() {
       </div>
     </div>
   );
+}
+
+export async function loader({ params }) {
+  console.log(params);
+  const { orderId } = params;
+  const order = await getOrder(orderId);
+  return order;
 }
 
 export default Order;
