d51d328b6daddc14c768b0fadc899953579911be
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-11 13:34:00 +0000

    implement error handling via react-router-com

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Cart from "./features/cart/Cart.jsx";
 import CreateOrder from "./features/order/CreateOrder.jsx";
 import Order from "./features/order/Order.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
+import Error from "./ui/Error.jsx";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
@@ -18,6 +20,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <Error />,
       },
       {
         path: "/cart",
