6cd3e80fe71aadd6725da3f9596208ffb775f7e7
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-11 13:17:31 +0000

    change app background colour

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="flex justify-between my-10">
+    <header className="flex justify-between my-10 text-[#f1f1f1]">
       <Link to="/">Stone Pizza Company</Link>
       <p>Cam Poole</p>
     </header>
