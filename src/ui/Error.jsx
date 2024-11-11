06cccc0c37fc0b2080d5085a1a5e1c2656ef8957
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-11 18:39:01 +0000

    fix background bug in parent error element

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -3,10 +3,9 @@ import { useNavigate, useRouteError } from "react-router-dom";
 function Error() {
   const navigate = useNavigate();
   const error = useRouteError();
-  console.log(error);
 
   return (
-    <div>
+    <div className="container mx-auto p-4 min-h-[calc(100vh-4rem)]">
       <h1>Something went wrong!</h1>
       <p>{error.data || error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
