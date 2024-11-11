dcb006784101dda3921b3d10694d4e7cfb6ed6ad
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-11 14:51:28 +0000

    add gradients to cartoverview and header backgrounds

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function CartOverview() {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-[#2f3343] text-white px-4 py-3 flex justify-between items-center shadow-lg">
+    <div className="fixed bottom-0 left-0 w-full bg-gradient-to-l to-[#494f62] from-[#1c1e25] text-white px-4 py-3 flex justify-between items-center shadow-lg">
       <p className="text-sm">
         <span className="font-semibold">3 pizzas</span>{" "}
         <span className="ml-2">£30.00</span>
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <header className="flex items-center bg-[#2f3343] py-3 lg:px-12 justify-between mb-9 text-[#f1f1f1]">
+    <header className="flex items-center bg-gradient-to-r to-[#494f62] from-[#1c1e25] py-3 lg:px-12 justify-between mb-9 text-[#f1f1f1]">
       <Link to="/">
         <img src="./StonePizza.svg" alt="logo" className="w-32 lg:w-40" />
       </Link>
