f8277bde66f533c1ef66e4c36d88e629025d617f
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-11 18:51:38 +0000

    fix relative path bug to logo image

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Logo from "../assets/StonePizza.svg";
 
 function Header() {
   return (
     <header className="flex items-center bg-gradient-to-r to-[#494f62] from-[#1c1e25] py-3 lg:px-12 justify-between mb-9 text-[#f1f1f1]">
       <Link to="/">
-        <img src="./StonePizza.svg" alt="logo" className="w-32 lg:w-36" />
+        <img src={Logo} alt="logo" className="w-32 lg:w-36" />
       </Link>
       <div className="flex gap-4 items-center">
         <SearchOrder />
