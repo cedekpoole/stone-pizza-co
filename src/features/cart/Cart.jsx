1d804748e4e350dd45d2a16a93fcdbd65b7ca462
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-12 12:05:28 +0000

    create reusable link button component

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom";
+import LinkButton from "../../ui/LinkButton";
+import Button from "../../ui/Button";
 
 function Cart() {
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <LinkButton to={"/menu"}>&larr; Back to menu</LinkButton>
 
       <h2>Your cart, %NAME%</h2>
 
       <div className="flex gap-4">
-        <Link to="/order/new">Order pizzas</Link>
+        <Link to="/order/new">
+          <Button>Order Pizzas</Button>
+        </Link>
         <button>Clear cart</button>
       </div>
     </div>
--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,14 +1,14 @@
-import { useNavigate, useRouteError } from "react-router-dom";
+import { useRouteError } from "react-router-dom";
+import LinkButton from "./LinkButton";
 
 function Error() {
-  const navigate = useNavigate();
   const error = useRouteError();
 
   return (
     <div className="container mx-auto p-4 min-h-[calc(100vh-4rem)]">
       <h1>Something went wrong!</h1>
       <p>{error.data || error.message}</p>
-      <button onClick={() => navigate(-1)}>&larr; Go back</button>
+      <LinkButton to="-1">&larr; Go back</LinkButton>
     </div>
   );
 }
