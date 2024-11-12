d15f2a16da0b73df05889c1599ea7c60fe2704af
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-12 15:46:53 +0000

    conditionally render createUser form

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,23 +1,30 @@
 import CreateUser from "../features/user/CreateUser";
 import Hero from "../assets/Hero.svg";
+import { useSelector } from "react-redux";
+import Button from "./Button";
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
   return (
-    <div className="flex flex-col sm:flex-row gap-4">
-      <div className="flex flex-col items-center gap-6 ml-0 sm:ml-3 md:ml-0">
-        <h1 className="pt-10 font-syne text-4xl sm:text-5xl font-bold">
+    <div className="flex flex-col gap-4 sm:flex-row">
+      <div className="ml-0 flex flex-col items-center gap-6 sm:ml-3 md:ml-0">
+        <h1 className="pt-10 font-syne text-4xl font-bold sm:text-5xl">
           Stone cold pizza...{" "}
-          <span className="block mt-3 sm:mt-5 text-[#BAB2E5]">
+          <span className="mt-3 block text-[#BAB2E5] sm:mt-5">
             Right to your doorstep.
           </span>
         </h1>
-        <CreateUser />
+        {username === "" ? (
+          <CreateUser />
+        ) : (
+          <Button to="/menu">Order Now</Button>
+        )}
       </div>
-      <div className="flex justify-center items-center w-full order-last sm:order-none mt-5 sm:mt-0">
+      <div className="order-last mt-5 flex w-full items-center justify-center sm:order-none sm:mt-0">
         <img
           src={Hero}
           alt="Hero"
-          className="w-8/12 h-8/12 object-contain mx-auto"
+          className="h-8/12 mx-auto w-8/12 object-contain"
         />
       </div>
     </div>
