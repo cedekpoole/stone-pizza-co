68c91465a534c6bfc50d7c436a205e75c55ba0c5
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-12 19:57:15 +0000

    edit focus on search input

--- src/features/order/SearchOrder.jsx
+++ src/features/order/SearchOrder.jsx
@@ -18,7 +18,7 @@ function SearchOrder() {
         value={query}
         placeholder="Search order #"
         onChange={(e) => setQuery(e.target.value)}
-        className="w-40 rounded-full bg-[#f1f1f1] px-2 py-1 text-white transition-all duration-500 focus:w-48 focus:bg-[#373c4b] focus:outline-none focus:ring focus:ring-[#373c4b] focus:ring-offset-2 sm:w-56 focus:sm:w-64"
+        className="w-40 rounded-full bg-[#f1f1f1] px-2 py-1 text-gray-400 transition-all duration-500 focus:w-48 focus:bg-[#373c4b] focus:outline-none focus:ring focus:ring-[#282c38] sm:w-56 focus:sm:w-64"
       />
     </form>
   );
