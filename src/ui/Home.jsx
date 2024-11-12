60b5748becde5dd01de3a27ceabd274c6e2c0cf4
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-12 12:44:42 +0000

    fix responsiveness of homepage

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -3,9 +3,9 @@ import Hero from "../assets/Hero.svg";
 
 function Home() {
   return (
-    <div className="flex gap-4">
-      <div className="flex flex-col gap-6">
-        <h1 className="pt-10 font-syne text-2xl sm:text-5xl font-bold">
+    <div className="flex flex-col sm:flex-row gap-4">
+      <div className="flex flex-col items-center gap-6 ml-0 sm:ml-3 md:ml-0">
+        <h1 className="pt-10 font-syne text-4xl sm:text-5xl font-bold">
           Stone cold pizza...{" "}
           <span className="block mt-3 sm:mt-5 text-[#BAB2E5]">
             Right to your doorstep.
@@ -13,11 +13,11 @@ function Home() {
         </h1>
         <CreateUser />
       </div>
-      <div>
+      <div className="flex justify-center items-center w-full order-last sm:order-none mt-5 sm:mt-0">
         <img
           src={Hero}
           alt="Hero"
-          className="w-8/12 h-8/12 col-span-2 object-contain mx-auto hidden sm:block"
+          className="w-8/12 h-8/12 object-contain mx-auto"
         />
       </div>
     </div>
