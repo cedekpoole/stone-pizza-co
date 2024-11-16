43ba6364b684928bce34f89b28b06262eabb3ef6
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-16 14:32:02 +0000

    flex-grow input fields

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -13,7 +13,7 @@ function Input({ type, name, defaultValue }) {
       name={name}
       defaultValue={defaultValue}
       required
-      className="rounded-full bg-[#f1f1f1] p-1 text-[#242424] transition-all duration-300 focus:outline-none focus:ring focus:ring-[#494f62] focus:ring-offset-1"
+      className="grow rounded-full bg-[#f1f1f1] p-1 text-[#242424] transition-all duration-300 focus:outline-none focus:ring focus:ring-[#494f62] focus:ring-offset-1"
     />
   );
 }
