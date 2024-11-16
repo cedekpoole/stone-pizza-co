653cec18c7cbebc2e20050761d3a88314fa308da
cedekpoole <cameron.edek.poole@gmail.com>
2024-11-16 14:40:36 +0000

    integrate geolocation

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -3,8 +3,8 @@ import { createOrder } from "../../services/apiMenu";
 import Logo from "../../assets/StonePizza.svg";
 import Input from "../../ui/Input";
 import Button from "../../ui/Button";
-import { useSelector } from "react-redux";
-import { getUsername } from "../user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchAddress } from "../user/userSlice";
 import { clearCart, getCart, getTotalCartPrice } from "../cart/cartSlice";
 import EmptyCart from "../cart/EmptyCart";
 import store from "../../store";
@@ -19,39 +19,65 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
+
   const cart = useSelector(getCart);
-  const username = useSelector(getUsername);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+  } = useSelector((state) => state.user);
+
+  const isLoadingAddress = addressStatus === "loading";
+
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
+
   const formErrors = useActionData();
+  const dispatch = useDispatch();
+
   const totalCartPrice = useSelector(getTotalCartPrice);
   const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
   const totalPrice = totalCartPrice + priorityPrice;
+
   if (cart.length === 0) return <EmptyCart />;
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3">
+    <div className="grid grid-cols-1 md:grid-cols-2">
       <div className="col-span-1 mx-auto mt-5">
         <h2 className="mb-7 font-syne text-2xl font-bold">Ready to order?</h2>
-
-        <Form method="POST" className="flex flex-col gap-6">
-          <div className="flex items-center gap-4 text-sm">
-            <label>FIRST NAME</label>
+        <Form method="POST" className="mb-5 flex flex-col gap-5">
+          <div className="flex flex-col gap-2 text-sm sm:flex-row sm:items-center">
+            <label className="sm:basis-30">FIRST NAME</label>
             <Input type={"text"} name={"customer"} defaultValue={username} />
           </div>
 
-          <div className="flex items-center gap-4 text-sm">
-            <label>PHONE NUMBER</label>
-            <div>
-              <Input type={"tel"} name="phone" />
-            </div>
+          <div className="flex flex-col gap-2 text-sm sm:flex-row sm:items-center">
+            <label className="sm:basis-30">PHONE NUMBER</label>
+            <Input type={"tel"} name="phone" />
           </div>
           {formErrors?.phone && <p className="text-sm">* {formErrors.phone}</p>}
-          <div className="flex items-center gap-4 text-sm">
-            <label>ADDRESS</label>
-            <div>
-              <Input type={"text"} name={"address"} />
-            </div>
+          <div className="flex flex-col gap-2 text-sm sm:flex-row sm:items-center">
+            <label className="sm:basis-30">ADDRESS</label>
+            <Input
+              disabled={isLoadingAddress}
+              defaultValue={address}
+              type={"text"}
+              name={"address"}
+            />
+
+            {!position.latitude && !position.longitude && (
+              <Button
+                type="light"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+                disabled={isLoadingAddress}
+              >
+                Get Location
+              </Button>
+            )}
           </div>
 
           <div className="text-md flex items-center gap-4">
@@ -78,7 +104,7 @@ function CreateOrder() {
       <img
         src={Logo}
         alt="Logo"
-        className="col-span-2 mx-auto hidden h-full w-full object-contain p-10 md:block"
+        className="col-span-1 mx-auto hidden h-full w-full object-contain p-1 pt-5 md:block"
       />
     </div>
   );
--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -4,16 +4,18 @@ Input.propTypes = {
   type: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   defaultValue: PropTypes.string,
+  disabled: PropTypes.bool,
 };
 
-function Input({ type, name, defaultValue }) {
+function Input({ type, name, defaultValue, disabled = false }) {
   return (
     <input
       type={type}
       name={name}
       defaultValue={defaultValue}
+      disabled={disabled}
       required
-      className="grow rounded-full bg-[#f1f1f1] p-1 text-[#242424] transition-all duration-300 focus:outline-none focus:ring focus:ring-[#494f62] focus:ring-offset-1"
+      className="grow rounded-full bg-[#f1f1f1] px-2 py-1 text-[#242424] transition-all duration-300 focus:outline-none focus:ring focus:ring-[#494f62] focus:ring-offset-1"
     />
   );
 }
